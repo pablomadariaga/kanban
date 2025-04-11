3d73aa0c1e0133f2db43bf64f74e9d4e0c073c68
Pablo <juanpablomadariagacardona@gmail.com>
2025-04-10 20:44:32 -0500

    add new trait for has card

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -2,15 +2,15 @@
 {{-- This view renders a full Kanban board with its states and cards. --}}
 {{-- It expects a $board variable (an instance of Pablomadariaga\Kanban\Models\Board) with loaded states and cards. --}}
 
-<div class="kanban-board flex space-x-4 overflow-x-auto p-4">
+<div class="kanban-board">
     @foreach ($board->states as $state)
-        <div class="kanban-column w-64 flex-shrink-0 rounded-md bg-gray-100">
+        <div class="kanban-column">
             {{-- State Column Header --}}
-            <div class="kanban-column-header px-3 py-2 font-bold text-gray-700">
+            <div class="kanban-column-header">
                 {{ $state->name }}
             </div>
             {{-- Cards List --}}
-            <ul class="kanban-cards flex flex-col gap-2 px-2 pb-2" x-data {{-- Initialize Alpine.js context for this column (not strictly needed for x-sort, but can be used if extended) --}}
+            <ul class="kanban-cards" x-data {{-- Initialize Alpine.js context for this column (not strictly needed for x-sort, but can be used if extended) --}}
                 x-sort.ghost="$wire.moveCard($item, {{ $state->id }}, $position)"
                 x-sort:group="'board-{{ $board->id }}'" wire:key="state-{{ $state->id }}">
                 @foreach ($state->cards as $card)
---
+++ src/Traits/HasKanbanCard.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Madariaga\Kanban\Traits;
+
+use Pablomadariaga\Kanban\Models\Card;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+/**
+ * Trait HasKanbanCard
+ *
+ * This trait allows an Eloquent model to be linked as a "cardable" entity in the Kanban system.
+ * By using this trait, the model will have a polymorphic one-to-one relationship with the Card model.
+ *
+ * Usage Example:
+ *
+ * ```php
+ * namespace App\Models;
+ *
+ * use Illuminate\Database\Eloquent\Model;
+ * use Madariaga\Kanban\Traits\HasKanbanCard;
+ *
+ * class Task extends Model
+ * {
+ *     use HasKanbanCard;
+ *
+ *     // Additional properties and methods
+ * }
+ * ```
+ *
+ * With this trait, you can easily access the related Kanban card using:
+ *
+ * ```php
+ * $task = Task::find(1);
+ * $kanbanCard = $task->kanbanCard; // Returns the associated card instance or null if none.
+ * ```
+ *
+ * @package Madariaga\Kanban\Traits
+ */
+trait HasKanbanCard
+{
+    /**
+     * Define a polymorphic one-to-one relationship.
+     *
+     * This method sets up the relationship linking the current model (as cardable) with a Card.
+     * It uses the columns "cardable_type" and "cardable_id" in the cards table.
+     *
+     * @return MorphOne<Card> Returns the associated Card model instance.
+     */
+    public function kanbanCard(): MorphOne
+    {
+        return $this->morphOne(Card::class, 'cardable');
+    }
+}
