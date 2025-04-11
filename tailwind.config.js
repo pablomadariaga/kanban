56cb527bbc6861390e6f70fe7daaa72a77f4a1df
Pablo <juanpablomadariagacardona@gmail.com>
2025-04-11 08:03:25 -0500

    fix v3 content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.php", "./src/**/*.js", "./src/**/*.blade.php"],
+  content: ["./**/*.php", "./src/**/*.js", "./**/**/*.blade.php"],
   darkMode: "class",
   theme: {},
   plugins: [require("./js/plugins/kaban")],
